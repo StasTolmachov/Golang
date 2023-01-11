81dabb26171d649e9d55c72bbc163bd9a76525f5
stastolmachov <stas@tolmachov.dev>
2023-01-11 06:01:10 +0100

    start

---
+++ Golang/HeadFirst/struct/struct.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	type subscriber struct {
+		name   string
+		rate   float64
+		active bool
+	}
+	var subscriber1 subscriber
+	subscriber1.name = "Adam"
+	subscriber1.rate = 4.99
+	subscriber1.active = true
+
+	var subscriber2 subscriber
+	subscriber2.name = "Beth"
+
+	fmt.Printf("%v%v\n", subscriber1, subscriber2)
+}
