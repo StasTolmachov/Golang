9f24a54f1fa18ac659d32645b3054ad5e84c7cc0
stastolmachov <stas@tolmachov.dev>
2023-01-16 21:48:09 +0100

    1

--- Golang/roulett/roulett.go
+++ Golang/roulett/roulett.go
@@ -1,23 +1,54 @@
+// игра в рулетку
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	var cash, wallet, betNumber, betRate, rouletteNumber, winNumber int
+	var cash, wallet, betRate, rouletteNumber, winNumber int
+	betNumbersArr := [34]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33}
+	var betNumberArr int
 
 	cash = 1000
 	wallet = cash
+	//начало цикла
+	for i := 1; wallet > 0; i++ {
+		fmt.Println("начался цикл пока кошелек не пустой", i)
+		betNumber := 55
+		fmt.Printf("Wallet: %v\n", wallet)
+		fmt.Printf("Input betNumber: ")
+		fmt.Scanln(&betNumber)
 
-	fmt.Println(wallet)
-	fmt.Scanln(&betNumber)
-	fmt.Scanln(&betRate)
-	wallet = wallet - betRate
-	rouletteNumber = 23
-	if rouletteNumber == betNumber {
-		winNumber = betRate * 5
-		wallet = wallet + winNumber
+		for betNumberArr = range betNumbersArr {
+			fmt.Println("начался цикл перебора чисел ставки", betNumberArr)
+			if betNumber == betNumberArr { //здесь вся игра
+				fmt.Println("если число совпало с массивом")
+				betRate = 0
+				fmt.Printf("Input betRate: ")
+				fmt.Scanln(&betRate)
+				wallet = wallet - betRate
+
+				//рандом нахождение числа рулетки
+				rand.Seed(time.Now().UnixNano())
+				min := 0
+				max := 33
+				rouletteNumber = rand.Intn(max-min+1) + min
+				fmt.Println(rouletteNumber)
+
+				//проверка выиграша
+				if rouletteNumber == betNumber {
+					winNumber = betRate * 5
+					wallet = wallet + winNumber
+					fmt.Printf("You Win: %v\n", winNumber)
+				}
+				break
+			}
+
+		}
+
 	}
 
-
-	fmt.Println(cash, wallet, betNumber)
 }
---
+++ Golang/test/test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	betNumber := 23
+	betNumbersArr := [34]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33}
+	var betNumberArr int
+
+	// for i := 0; i < len(betNumberArr); i++ {
+	// 	fmt.Println("df")
+	// }
+
+	for betNumberArr = range betNumbersArr {
+		if betNumber == betNumberArr {
+			fmt.Println("game start")
+		}
+	}
+	// fmt.Printf("%v\n%v\n", betNumberArr, len(betNumberArr))
+
+	var test int
+	
+	fmt.Println(test)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+}
