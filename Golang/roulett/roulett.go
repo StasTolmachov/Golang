6842abdd27d865a58c5f017b1cd77d21275ec5a5
stastolmachov <stas@tolmachov.dev>
2023-01-23 00:19:43 +0100

    222

--- Golang/roulett/roulett.go
+++ Golang/roulett/roulett.go
@@ -8,56 +8,69 @@ import (
 )
 
 func main() {
-	var cash, wallet, betRate, rouletteNumber, winNumber int
-	betNumbersArr := [34]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33}
-	var betNumberArr int
-
+	var cash int        //начальная сумма
+	var wallet int      // кошелек игрока
+	var betColor string // цвет ставки
+	var betNum int      // число ставки
+	var betRate int     // размер ставки
+	var roulettNum int  //число выпало на рулетке
+	var winSum int      //сумма выиграша
+	betNumbersArr := []int{0, 1, 2, 3}
+	var betNumArr int //число допустимой ставки
 	cash = 1000
 	wallet = cash
 
-	for i := 1; wallet > 0; i++ { //начало цикла
+	for i := 1; wallet > 0; i++ { //начало цикла игры со счетчиком
 		fmt.Println("начался цикл пока кошелек не пустой", i)
-		betNumber := 55
-		fmt.Printf("Wallet: %v\n", wallet)
-		fmt.Printf("Input betNumber: ")
-		fmt.Scanln(&betNumber)
 
-		if betNumber == 66 { //exit
-			panic(betNumber)
+		// цикл ввода цвета ставки
+		fmt.Println("Введите цвет ставки или выход")
+		for { //цикл ввода цвета ставки только допустимых значений
+			fmt.Scan(&betColor)
+			if betColor == "red" {
+				break
+			}
+			if betColor == "black" {
+				break
+			}
+			if betColor == "exit" {
+				break
+			}
+			fmt.Println("неверный ввод")
+
+		}
+		if betColor == "exit" {
+			break
 		}
 
-		for betNumberArr = range betNumbersArr {
-			fmt.Println("начался цикл перебора чисел ставки", betNumberArr)
-			if betNumber == betNumberArr { //здесь вся игра
-				fmt.Println("если число совпало с массивом")
-				betRate = 0
-				fmt.Printf("Input betRate: ")
-				_, err := fmt.Scanf("%d", &betRate)
-				if err == nil {
-
-					if betRate < wallet {
-						wallet = wallet - betRate
-
-						rand.Seed(time.Now().UnixNano()) //рандом нахождение числа рулетки
-						min := 0
-						max := 33
-						rouletteNumber = rand.Intn(max-min+1) + min
-						fmt.Println("rouletteNumber", rouletteNumber)
-
-						if rouletteNumber == betNumber { //проверка выиграша
-							winNumber = betRate * 5
-							wallet = wallet + winNumber
-							fmt.Printf("You Win: %v\n", winNumber)
-						}
-						fmt.Println("you lost")
-						break
-					}
+		fmt.Printf("Input betNum: ")
+		for { //цикл ввода числа ставки допустимых значений
+			fmt.Println("введите число ставки")
+			for _, betNumArr = range betNumbersArr {
+				if betNum == betNumArr {
+					break
 				}
-
 			}
+			if betNum == betNumArr {
+				break
+			}
+			fmt.Println("неверный ввод")
+		}
 
+		fmt.Printf("Input betRate: ") //ввод размера ставки
+
+		rand.Seed(time.Now().UnixNano()) //рандом нахождение числа рулетки
+		min := 0
+		max := 33
+		roulettNum = rand.Intn(max-min+1) + min
+		fmt.Println("roulettNum", roulettNum)
+
+		if roulettNum == betNum { //проверка выиграша
+			winSum = betRate * 2
+			wallet += winSum
+			fmt.Printf("You Win: %v\n", winSum)
 		}
 
 	}
-
+	fmt.Println(betColor, betNumbersArr)
 }
--- Golang/test/test.go
+++ Golang/test/test.go
@@ -3,20 +3,48 @@ package main
 import "fmt"
 
 func main() {
-	var betNum int
-	wallet := 20
-	for wallet > 0 { //цикл всей игры
-		fmt.Println("начало цикла")
-		for { //цикл проверки ввода данных
-			_, err := fmt.Scanf("%d", &betNum)
-			wallet -= betNum
-			if err == nil {
+	// var betNum int
+	// wallet := 20
+	// for wallet > 0 { //цикл всей игры
+	// 	fmt.Println("начало цикла")
+	// 	for { //цикл проверки ввода данных
+	// 		_, err := fmt.Scanf("%d", &betNum)
+	// 		wallet -= betNum
+	// 		if err == nil {
+	// 			break
+	// 		}
+	// 	}
+	// 	fmt.Println("play")
+	// }
+	// fmt.Println("выход с цыкла игры", betNum, wallet)
+
+	betNumbersArr := []int{0, 1, 2}
+	var betNum int // число ставки
+	var betNumArr int
+	var temp int
+	// betNum = 1
+
+	for {
+		fmt.Println("введите число ставки")
+		_, err := fmt.Scanf("%d", &betNum)
+		if err != nil {
+			fmt.Println("неправельный ввод err", err)
+			err = nil
+			continue
+		}
+		for _, betNumArr = range betNumbersArr {
+			if betNum == betNumArr {
 				break
 			}
 		}
-		fmt.Println("play")
+		if betNum == betNumArr {
+			break
+		}
+		fmt.Println("неправельный ввод")
 	}
-	fmt.Println("выход с цыкла игры", betNum, wallet)
+
+	fmt.Println(betNum, betNumArr, temp)
 }
 
 // fmt.Println("неправельный ввод")
+// betNumbersArr := [34]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33}
