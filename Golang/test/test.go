0639e8cd25b9155908e2c1cd759678832a3efdd8
stastolmachov <stas@tolmachov.dev>
2023-01-24 19:03:01 +0100

    24

--- Golang/test/test.go
+++ Golang/test/test.go
@@ -1,53 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	// var betNum int
-	// wallet := 20
-	// for wallet > 0 { //цикл всей игры
-	// 	fmt.Println("начало цикла")
-	// 	for { //цикл проверки ввода данных
-	// 		_, err := fmt.Scanf("%d", &betNum)
-	// 		wallet -= betNum
-	// 		if err == nil {
-	// 			break
-	// 		}
-	// 	}
-	// 	fmt.Println("play")
-	// }
-	// fmt.Println("выход с цыкла игры", betNum, wallet)
-
-	betNumbersArr := []int{0, 1, 2}
-	var betNum int // число ставки
-	var betNumArr int
-	var temp int
-	// betNum = 1
-
-	for {
-		fmt.Println("введите число ставки")
-		fmt.Scanln(&betNum)
-		// fmt.Scanf("%d", &betNum)
-		// if err != nil {
-		// 	fmt.Println("неправельный ввод err", err)
-
-		// 	continue
-		// }
-		for _, betNumArr = range betNumbersArr {
-			if betNum == betNumArr {
-				break
-			}
-		}
-		if betNum == betNumArr {
+	var betColor string
+	fmt.Println("Введите цвет ставки или выход")
+	for { //цикл ввода цвета ставки только допустимых значений
+		fmt.Scan(&betColor)
+		if betColor == "red" {
 			break
 		}
-		fmt.Println("неправельный ввод")
+		if betColor == "black" {
+			break
+		}
+		if betColor == "exit" {
+			break
+		}
+		fmt.Println("неверный ввод")
+
 	}
-
-	fmt.Println(betNum, betNumArr, temp)
+	fmt.Println(betColor)
 }
-
-// fmt.Println("неправельный ввод")
-// betNumbersArr := [34]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33}
