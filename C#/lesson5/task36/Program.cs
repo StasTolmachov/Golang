d80f8a2d34cd778b84b068a92b25e9b864a89343
StasTolmachov <Stas@Tolmachov.dev>
2023-03-13 15:38:04 +0100

    lesson5

---
+++ C#/lesson5/task36/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[4];
+        Random random = new Random();
+
+        Console.Write("Сгенерированный массив: [");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(-10, 10);
+            Console.Write(array[i]);
+            if (i != array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+        int sum = 0;
+        for (int i = 0; i < array.Length; i += 2)
+        {
+            sum += array[i];
+        }
+
+        Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + sum);
+
+        Console.ReadKey();
+    }
+}
