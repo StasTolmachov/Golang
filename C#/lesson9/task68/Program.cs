0ce728a052775cb9b0f144af749db61956aed232
StasTolmachov <Stas@Tolmachov.dev>
2023-03-13 16:18:40 +0100

    lesson9

---
+++ C#/lesson9/task68/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int m = 60;
+        int n = 48;
+
+        int gcd = Gcd(m, n);
+
+        Console.WriteLine($"НОД({m}, {n}) = {gcd}");
+    }
+
+    static int Gcd(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return Gcd(b, a % b);
+        }
+    }
+}
