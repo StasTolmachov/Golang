78170ede6a11483a57b54cbe2406e264728125a2
StasTolmachov <Stas@Tolmachov.dev>
2023-03-13 15:00:41 +0100

    lesson2

---
+++ C#/lesson2/task13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Получаем третью цифру числа, деля его на 100 и беря остаток от деления на 10
+        int thirdDigit = (number / 100) % 10;
+
+        if (thirdDigit != 0)
+        {
+            Console.WriteLine("Третья цифра числа: " + thirdDigit);
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    }
+}
