517867b3f142bf97b7ee4d6c1d336d57fd187586
StasTolmachov <Stas@Tolmachov.dev>
2023-03-13 15:25:58 +0100

    lesson4

---
+++ C#/lesson4/task29/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 5, 7, 19 };
+
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i != array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+        Console.ReadKey();
+    }
+}
