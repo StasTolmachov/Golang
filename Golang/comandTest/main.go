facd84b43077c0a81a9cdf3e5ca1e10b4b8a1674
stastolmachov <stas@tolmachov.dev>
2023-03-16 13:47:28 +0100

    'test'

--- Golang/comandTest/main.go
+++ Golang/comandTest/main.go
@@ -16,10 +16,10 @@ func main() {
 	cmd = exec.Command("git", "commit", "-m", "'test'")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
-	cmd = exec.Command("git", "status")
+	cmd = exec.Command("git", "push")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
-	cmd = exec.Command("git", "push")
+	cmd = exec.Command("git", "status")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 }
