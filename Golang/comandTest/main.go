07c4e6e71f67c3f93a77b155c41fcc266e79576f
stastolmachov <stas@tolmachov.dev>
2023-03-16 13:45:10 +0100

    'test'

---
+++ Golang/comandTest/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+)
+
+func main() {
+	// cmd := exec.Command("clear")
+	cmd := exec.Command("git", "status")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	cmd = exec.Command("git", "add", ".")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	cmd = exec.Command("git", "commit", "-m", "'test'")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
