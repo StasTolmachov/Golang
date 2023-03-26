424d137db75b37ebb82a15952c148354f47eacf5
StasTolmachov <Stas@Tolmachov.dev>
2023-03-26 14:07:52 +0200

    Проходим по всем элементам исходного массива

--- C#/specializationFinalProject/Program.cs
+++ C#/specializationFinalProject/Program.cs
@@ -10,5 +10,16 @@ class Program
         // Создаем массив для хранения строк длиной <= 3 символов
         string[] result = new string[source.Length];
         int count = 0;
+
+        // Проходим по всем элементам исходного массива
+        for (int i = 0; i < source.Length; i++)
+        {
+            // Если длина строки <= 3 символов, то добавляем ее в результирующий массив
+            if (source[i].Length <= 3)
+            {
+                result[count] = source[i];
+                count++;
+            }
+        }
     }
 }
