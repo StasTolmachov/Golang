7551ffd30f5e71476dbae772749c03f037057860
StasTolmachov <Stas@Tolmachov.dev>
2023-03-26 14:07:03 +0200

    Создаем массив для хранения строк

---
+++ C#/specializationFinalProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Исходный массив строк
+        string[] source = { "one", "two", "three", "four", "five", "six" };
+
+        // Создаем массив для хранения строк длиной <= 3 символов
+        string[] result = new string[source.Length];
+        int count = 0;
+    }
+}
