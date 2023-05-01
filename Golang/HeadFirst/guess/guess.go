8a6759a96124c33b85dce18c2856ff3988985bb3
Stas <107077607+StasTolmachov@users.noreply.github.com>
2023-05-01 14:23:25 +0200

    guess

--- Golang/HeadFirst/guess/guess.go
+++ Golang/HeadFirst/guess/guess.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	success := false
+	// генерация рандомного числа
+	target := rand.Intn(100) + 1
+
+	for guesses := 0; guesses < 10; guesses++ {
+		// ввод пользователя
+		fmt.Print("Enter your number: ")
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Panic(err)
+		}
+		input = strings.TrimSpace(input)
+
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Panic(err)
+		}
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
+	}
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
+	}
 }
