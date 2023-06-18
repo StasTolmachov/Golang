fe8f212150830e4f60a3393725e961b9dcea7a84
Stas <107077607+StasTolmachov@users.noreply.github.com>
2023-06-18 18:16:12 +0200

    finished Head First

--- js/headFirst/tolmachov/chapter9/main.js
+++ js/headFirst/tolmachov/chapter9/main.js
@@ -1,5 +1,52 @@
-window.onload = pageLoaderHandler()
+window.onload = init
 
-function pageLoaderHandler() {
-    console.log("worked")
+function init() {
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        images[i].onclick = showAnswer;
+    }
 }
+
+function showAnswer(eventObj) {
+    var image = eventObj.target;
+    var name = image.id;
+    name = name + ".jpg";
+    image.src = name;
+    setTimeout(reblur, 2000, image)
+}
+
+function reblur(image) {
+    var name = image.id
+    name = name + "blur.jpg"
+    image.src = name
+}
+
+function Dog(name, breed) {
+    this.name = name
+    this.breed = breed
+}
+
+Dog.prototype.run = function () {
+    console.log("Run")
+}
+Dog.prototype.run2 = function () {
+    console.log("Run2")
+}
+
+var dog1 = new Dog("dog1", "Dvor")
+var dog2 = new Dog("dog2", "Dvor")
+
+console.log(dog1, dog2)
+dog1.run()
+
+function ShowDog(name, breed, v) {
+    this.name = name
+    this.breed = breed
+    this.v = v
+}
+ShowDog.prototype = new Dog()
+
+var showDog1 = new ShowDog("showDog1", "showDog1", "showDog1")
+
+showDog1.run()
+showDog1.run2()
